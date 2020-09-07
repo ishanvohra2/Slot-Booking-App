dcd1a5289fdd32aabebfe93921eec5d93b2530bc
ishanvohra2 <ishanvohra2@gmail.com>
2020-09-07 13:14:10 +0530

    Horizontal Calendar view added

--- app/src/main/java/com/ishanvohra/slotbooking/MainActivity.java
+++ app/src/main/java/com/ishanvohra/slotbooking/MainActivity.java
@@ -3,6 +3,12 @@ package com.ishanvohra.slotbooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Start date for calendar
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, 0);
+
+        //End date for calendar
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 10);
+
+        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.main_activity_calendar_view)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .build();
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+
+            }
+        });
     }
 }
